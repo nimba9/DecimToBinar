50ca19a624dd41ce4872e23d7eae1911f9c0b7b6
vancisin peter <vancisin.peter@gmail.com>
2017-04-17 23:58:06 +0200

    Add project files.

---
+++ nakovChp06DecimToBinar/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nakovChp06DecimToBinar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a positive number: ");
+
+
+            string userInp = Console.ReadLine();
+            int num = 0;
+
+            bool check = int.TryParse(userInp, out num);
+
+            if (check != true)
+            {
+                Console.WriteLine("Please enter a positive integer");
+
+            }
+            Console.WriteLine("the number is {0}", num);
+
+            int division = 0;
+
+            string remainder = string.Empty;
+
+            while (num >= 1)
+
+            {
+                division = num / 2;
+                remainder += (num % 2).ToString();
+                num = division;
+            }
+
+            string binary = string.Empty;
+
+            for (int i = remainder.Length - 1; i >= 0; i--)
+            {
+                binary = binary + remainder[i];
+            }
+
+            Console.WriteLine("The binary version of the entered number is {0}", binary );
+        }
+    }
+}
